789c02b41731d14ea717af2f040738071d7141e5
Ingo Lange <lange.data.science@gmail.com>
2024-04-06 22:46:28 +0200

    Dummy commit to test CICD

--- components/Specific/Skills.tsx
+++ components/Specific/Skills.tsx
@@ -3,7 +3,7 @@
 function Skills() {
   return (
     <div className="w-full bg-opacity-90 bg-black grid grid-cols-2 mt-12 p-8 rounded-lg">
-      Skills to be added
+      Skills to be added here
     </div>
   );
 }
